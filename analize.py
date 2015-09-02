b35f35f13f4e09d1b7988fc48a7ede7081dc75fa
DoctaDocta <trashm96@gmail.com>
2015-09-02 18:32:36 -0500

    improved flow control, added print statements

--- analize.py
+++ analize.py
@@ -46,45 +46,58 @@ class Case:
 
 samples = [];
 samplesLinkedToFiles = {};
+fileInCabinet = False;
+placeHolder = -1;
 
 # reading in the RNA file_manifest
 i = 0;
 with open('TCGA CHOL RNA-seq/file_manifest.txt', 'rb') as f:
 	reader = csv.reader(f, delimiter="\t")
+	print "reading in file_manifest, now iterating through line by line..."
 	for line in reader:
 		i = i+1
-		if "TCGA" in line[5]:
-			samples.append(line[5]) #not necassary
+		for bb in cabinet:
+			print "current state of the cabinet: ", bb.sample
+		if "TCGA" in line[5]: #removed the top three rows of manifest b/c unnecessary
+			print "current sample", i,"from manifest: "
+			print "sample: ", line[5], "barcode_filename: ",line[6] #not necessary
+			samples.append(line[5]) #not necassary but shines a light on sample names
 			for l in cabinet:
-				print "current case: ", l.sample
-				if (line[5] != l.sample):
-					x = Case(line[5], 'none')
-					if "genes.normalized" in line[6]:
-						x.genes_norms(line[6])
-					elif "genes.results" in line[6]:
-						x.genes_results(line[6])
-					elif "isoforms.results" in line[6]:
-						x.isoforms_results(line[6])
-					elif "isoforms.normalized" in line[6]:
-						x.isoforms_norms(line[6])
-					cabinet.append(x)
-				else:
-					placeHolder = cabinet.index(line[5])
+				print "searching cabinet..."
+				print "current case in Cabinet: ", l.sample
+				if (line[5] == l.sample): #the sample is already in cabinet
+					fileInCabinet = True;
+					placeHolder = cabinet.index(line[5]); #find position of the Case
 					print "placeHolder: ", placeHolder
-					if "genes.normalized" in line[6]:
-						cabinet[placeHolder].genes_norms(line[6])
-					if "genes.results" in line[6]:
-						cabinet[placeHolder].genes_results(line[6])
-					if "isoforms.normalized" in line[6]:
-						cabinet[placeHolder].genes_norms(line[6])
-					if "isoforms.results" in line[6]:
-						cabinet[placeHolder].genes_norms(line[6])
+			if (fileInCabinet == True): #this needs to be after the for loop
+										#b/c we search the whole cabinet
+				print "found the case in cabinet"
+				if "genes.normalized" in line[6]:
+					cabinet[placeHolder].genes_norms(line[6])
+				if "genes.results" in line[6]:
+					cabinet[placeHolder].genes_results(line[6])
+				if "isoforms.normalized" in line[6]:
+					cabinet[placeHolder].genes_norms(line[6])
+				if "isoforms.results" in line[6]:
+					cabinet[placeHolder].genes_norms(line[6])
+			else: #the sample is not already in Cabinet... (fileInCabinet == false)
+				print "the case wasn't in cabinet"
+				x = Case(line[5], 'none')
+				if "genes.normalized" in line[6]:
+					x.genes_norms(line[6])
+				elif "genes.results" in line[6]:
+					x.genes_results(line[6])
+				elif "isoforms.results" in line[6]:
+					x.isoforms_results(line[6])
+				elif "isoforms.normalized" in line[6]:
+					x.isoforms_norms(line[6])
+				cabinet.append(x)
+		else:
+			print "current sample",i, "or barcode_filename from manifest... is not needed"
 		#cabinet.append(x)
-		print "sample: ", line[5], "barcode_filename: ",line[6] #not necessary
-
 
 print "whole list of samples: ", len(samples), "verified by lines in RNA manifest: ", i/6
 setSamples = list(set(samples))
 for l in setSamples:
-	print "sample in array 'samples': ", l
+	print "sample name in array 'samples': ", l
 print "simplified list of samples: ", len(setSamples)
