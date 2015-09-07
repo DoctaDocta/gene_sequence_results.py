10a402be0eefee7a6d2d9eaa82ab83ac9add8b67
DoctaDocta <trashm96@gmail.com>
2015-09-07 17:56:44 -0400

    read in results using object function instead of in for loop

--- analize.py
+++ analize.py
@@ -49,17 +49,33 @@ class TissueSample:
 	tissue_type = "none";
 	def add_tissue_type(self,tt):
 		self.tissue_type = tt;
-	def add_isoforms_norms_file(self,isoforms_norms_file): #functions to add attribute to the object...
+	def add_isoforms_norms(self,isoforms_norms_file): #functions to add attribute to the object...
 		path = pathToDataFiles + "/" + isoforms_norms_file;# filename passed from manifest needs it's path prepended.
 		self.isoforms_norms['filename'] = path; 			# so we use functions for the name, but when adding genes
-															#we'll do it like this  genes_norms['gene_id'] = line[1].
-	def add_genes_norms_file(self,genes_norms_file):
+		with open(path, 'rb') as f:
+			print "\n\nReading in Isoforms Normalized Results and storing in TissueSample in cabinet."
+			reader = csv.reader(f, delimiter="\t")
+			for line in reader:
+				self.isoforms_norms[line[0]] = line[1]
+
+	def add_genes_norms(self,genes_norms_file):
 		path = pathToDataFiles + "/" + genes_norms_file;
 		self.genes_norms['filename'] = path;
+		with open(path, 'rb') as f:
+			print "\n\nReading in Genes Normalized Results and storing in TissueSample."
+			reader = csv.reader(f, delimiter="\t")
+			for line in reader:
+				self.genes_norms[line[0]] = line[1]
 
-	def add_genes_results_file(self,genes_results_file):
+	def add_genes_results(self,genes_results_file):
 		path = pathToDataFiles + "/" + genes_results_file;
 		self.genes_results['filename'] = path;
+		with open(path, 'rb') as f:
+			print "\n\nReading in Gene Results and adding to TissueSample in cabinet"
+			reader = csv.reader(f, delimiter="\t")
+			for line in reader:
+				arrayify = line[3].split(",")
+				self.genes_results[line[0]] = arrayify;
 # you can add attributes (or attr's) to your cases by defining (or def) functions to input
 # learned from: http://sthurlow.com/python/lesson08/
 
@@ -89,70 +105,30 @@ with open(manifestFile, 'rb') as f:
 			if (fileInCabinet == True): #this needs to be after the for loop searching cabinet.
 										#b/c we search the whole cabinet for the file.
 				if "genes.normalized" in line[6]:
-					cabinet[placeHolder].add_genes_norms_file(line[6])
-					filePath = cabinet[placeHolder].genes_norms['filename']
-					with open(filePath, 'rb') as f:
-						print "\n\nReading in Gene Normalized Results and storing in TissueSample."
-						reader = csv.reader(f, delimiter="\t")
-						for line in reader:
-							cabinet[placeHolder].genes_norms[line[0]] = line[1]
-					print "\t\tGenes_norms added to TissueSample in cabinet"
+					cabinet[placeHolder].add_genes_norms(line[6])
 
 				elif "genes.results" in line[6]:
-					cabinet[placeHolder].add_genes_results_file(line[6]);
-					filePath = cabinet[placeHolder].genes_results['filename']
-					with open(filePath, 'rb') as f:
-						print "\n\nReading in Gene Results"
-						reader = csv.reader(f, delimiter="\t")
-						for line in reader:
-							arrayify = line[3].split(",")
-							cabinet[placeHolder].genes_results[line[0]] = arrayify;
-					print "\t\tgenes_results['filename'] added to TissueSample in cabinet"
+					cabinet[placeHolder].add_genes_results(line[6]);
 
 				elif "isoforms.normalized" in line[6]:
-					cabinet[placeHolder].add_isoforms_norms_file(line[6]);
-					filePath = cabinet[placeHolder].isoforms_norms['filename']
-					with open(filePath, 'rb') as f:
-						print "\n\nReading in Isoforms Normalized Results and storing in TissueSample."
-						reader = csv.reader(f, delimiter="\t")
-						for line in reader:
-							cabinet[placeHolder].isoforms_norms[line[0]] = line[1]
-					print "\t\tisoforms_norms added to TissueSample in cabinet"
+					cabinet[placeHolder].add_isoforms_norms(line[6]);
+
 				else:
 					print "\t\tthe associated barcode_filename is not needed"
 				fileInCabinet = False #now search is done, reset this indicator for next sample.
 			else: #the sample is not in Cabinet.
-				print "\t\tthe TissueSample wasn't found in cabinet"
 				x = TissueSample(line[5]) #We make an new instance of the object
-				print "\t\tAdding new case with sample", x.sample, " to cabinet"
+
+				print "\t\tTissueSample wasn't found in cabinet. Adding new case with sample", x.sample, " to cabinet"
 				if "genes.normalized" in line[6]:
-					x.add_genes_norms_file(line[6])
-					filePath = x.genes_norms['filename']
-					with open(filePath, 'rb') as f:
-						print "\n\nReading in Gene Normalized Results and storing in TissueSample."
-						reader = csv.reader(f, delimiter="\t")
-						for line in reader:
-							x.genes_norms[line[0]] = line[1] # give the object an attribute
-					print "\t\twith genes_norms['']", x.sample
+					x.add_genes_norms(line[6])
+
 				elif "genes.results" in line[6]:
-					x.add_genes_results_file(line[6])
-					filePath = x.genes_results['filename']
-					with open(filePath, 'rb') as f:
-						print "\n\nReading in Gene Results"
-						reader = csv.reader(f, delimiter="\t")
-						for line in reader:
-							arrayify = line[3].split(",")
-							x.genes_results[line[0]] = arrayify;
-					print "\t\twith genes_results['filename']", x.sample
+					x.add_genes_results(line[6])
+
 				elif "isoforms.normalized" in line[6]:
-					x.add_isoforms_norms_file(line[6])
-					filePath = x.isoforms_norms['filename']
-					with open(filePath, 'rb') as f:
-						print "\n\nReading in Isoforms Normalized Results and storing in TissueSample."
-						reader = csv.reader(f, delimiter="\t")
-						for line in reader:
-							x.isoforms_norms[line[0]] = line[1]
-					print "\t\twith isoforms_norms_file", x.sample
+					x.add_isoforms_norms(line[6])
+
 				cabinet.append(x) #and add it to the cabinet.
 		else: #this means the string "rsem." is not in line[6], therefore the line isn't important for our purposes.
 			print "current row",i,"does not contain a sample or proper barcode_filename\n"
@@ -177,33 +153,8 @@ with open(tissue_type_file, 'rb') as f:
 					ass.add_tissue_type(line[1])
 					print 'added tissue tupe to TissueSample'
 
-#HOW TO SEARCH THRU CABINET AND STORE VALS IN GROUPS
-groupA = []; #this will be a list of lists. ex. ['[red,blue,green]','[orange, 'yellow', 'berries']','['cheery', 'moody', 'peepish']''
-groupB = [];
-tmpList = []; #this will be the new line
 
 
-#this takes way too long. haven't seen it even complete...
-'''
-for l in cabinet:
-	print l.sample, l.tissue_type
-	for geneid, normcount in l.genes_norms.iteritems():
-		if (normcount > 2):
-			# MAKE STRING OF INFORMATION
-			# GENEID, NORMCOUNT, SAMPLE, TISSUE TYPE.
-			tmpList.extend((geneid,normcount,l.sample,l.tissue_type)) #append multiple items
-			print tmpList, "to groupA"
-			groupA.extend(tmpList)
-		else:
-			tmpList.extend((geneid,normcount,l.sample,l.tissue_type)) #append multiple items
-			print tmpList, "to groupB"
-			groupB.extend(tmpList)
-'''
-for l in cabinet:
-	max_key = max(l.genes_results, key= lambda x: len(set(l.genes_results[x])))
-
-
-'''
 #Printing the cabinet to console!
 print "\n\nOverview of Filing Cabinet w/ shortened filenames..."
 print '-' * 55
@@ -219,7 +170,6 @@ print "currCase: ", currCase.sample, "tissue_type:", currCase.tissue_type
 print "Displaying gene_id's and their normalized_counts for this sample."
 for key, val in currCase.genes_norms.iteritems():
 	print "gene_id:",key, "normalized_count:",val, "sample:",currCase.sample, "tissue_type:",currCase.tissue_type
-'''
 
 # instead of storing those values in python,
 # i want to output them into files that he can read with R or Perl...
