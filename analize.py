33863ce33b187aa1977699edf9d6f7f3cf6d0280
DoctaDocta <trashm96@gmail.com>
2015-09-11 00:30:55 -0400

    dotted some i's

--- analize.py
+++ analize.py
@@ -61,86 +61,6 @@ filenameKeywords = ["genes.results","genes.normalized","isoforms.normalized"];
 tissue_type_file = "tcga CHOL sample bar code_tissuetype.csv";
 
 
-
-geneIds = [];
-tmpList = [];
-
-# This is a class definition, an object called TissueSample that will hold all information for the barcode.
-class TissueSample:
-	def __init__(self,barcode): # function initializes class, must pass in a barcode and tissue type.
-		self.barcode = barcode;				#see example below class definition
-		self.sample = barcode[:15] #the sample is first 15 char, see manifest for example.
-	#attributes for the TissueSample. These hold the filenames for data, and will hold data themselves.
-	genes_results = {"gene_id":"multiple_transcript_ids", #attribute  with dictionary
-					"filename": ""}
-	genes_norms = 	{"gene_id":"normalized_count",
-					"filename": ""}
-	isoforms_norms = {"isoform_id":"normalized_count", #isoform_id == transcript id from gene.results
-						"filename": ""}
-	tissue_type = "none";
-
-	def find_tissue_type(self):
-		self.tissue_type = tissueTypeDict[self.sample];
-		print "\t\tFound tissue type.", self.barcode, self.tissue_type, "Added to cabinet!"
-
-	'''def add_isoforms_ids(self):
-		with open(self.genes_results['filename'], 'rb') as f: #use the file we read in to get all its necessary contents
-				print "\t\tReading in corresponding Genes and Isoforms IDs, and each iso_id normcount. Writing to Database."
-				reader = csv.reader(f, delimiter="\t")
-				for line in reader:
-					arrayify = line[3].split(",") #arrafiy is a list of transcript ids. they match to isoforms norms file.
-					self.genes_results[line[0]] = arrayify;
-	'''
-	def add_isoforms_norms(self,isoforms_norms_file): #functions to add attribute to the object...
-		path = pathToDataFiles + isoforms_norms_file;# filename passed from manifest needs it's path prepended.
-		self.isoforms_norms['filename'] = path; 			# so we use functions for the name, but when adding genes
-		'''if (self.genes_results['filename']):
-			with open(path, 'rb') as f: #use the file we read in to get all its necessary contents
-				print "\t\tReading in Isoforms Normalized Results and storing in TissueSample in cabinet."
-				reader = csv.reader(f, delimiter="\t")
-				for line in reader:
-					self.isoforms_norms[line[0]] = line[1] #add this data to the dict attribute above
-		'''
-	def add_genes_norms(self,genes_norms_file):
-		path = pathToDataFiles + genes_norms_file;
-		self.genes_norms['filename'] = path;
-		with open(path, 'rb') as f:
-			print "\t\tReading in Genes Normalized Results and executing to DB. This may take a while..."
-			reader = csv.reader(f, delimiter="\t")
-			tmpList = [];
-			for line in reader:
-				tmpList = [];
-				#self.genes_norms[line[0]] = line[1]
-				tmpList.extend([line[0],line[1],self.barcode,self.tissue_type]);
-				#print tmpList
-				cur.execute('INSERT INTO genes VALUES (?,?,?,?)', tmpList)
-				#con.commit()
-		#cur.execute("SELECT * FROM genes")
-		#rows = cur.fetchall()
-		#for row in rows:
-		#	print row
-		#this is where we will export our fulfilled TissueSample object to a table using sqlite.
-
-	def add_genes_results(self,genes_results_file):
-		path = pathToDataFiles + genes_results_file;
-		self.genes_results['filename'] = path;
-		'''with open(path, 'rb') as f:
-			print "\t\tReading in Gene Results and adding to TissueSample in cabinet"
-			reader = csv.reader(f, delimiter="\t")
-			if (self.isoforms_norms['filename']):
-				for line in reader:
-					arrayify = line[3].split(",") #arrafiy is a list of transcript ids. they match to isoforms norms file.
-
-					self.genes_results([line[0]]) = {isoform_id[]}
-			else:
-				for line in reader:
-					arrayify = line[3].split(",") #arrafiy is a list of transcript ids. they match to isoforms norms file.
-					self.genes_results[line[0]] = arrayify;
-		'''
-
-# you can add attributes (or attr's) to your cases by defining (or def) functions to input
-# learned from: http://sthurlow.com/python/lesson08/
-
 ########################################################
 #Reading Samples: Tissue Types. Adding to dictionary.###
 ########################################################
@@ -162,6 +82,86 @@ with open(tissue_type_file, 'rb') as f:
 			tissue = "reset"
 		ctr = ctr + 1;
 
+
+tmpList = []; #just preloading. this will be used to populate the db file! see the class
+
+# This is a class definition, an object called TissueSample that will hold all information for the barcode.
+class TissueSample:
+	def __init__(self,barcode): # function initializes class, must pass in a barcode and tissue type.
+		self.barcode = barcode;				#see example below class definition
+		self.sample = barcode[:15] #the sample is first 15 char, see manifest for example.
+		self.tissue_type = tissueTypeDict[self.sample];
+		print "\t\tFound tissue type:", self.barcode, self.tissue_type, "Added to cabinet!"
+	#attributes for the TissueSample. These hold the filenames for data, and will hold data themselves.
+	genes_results = {"gene_id":"multiple_transcript_ids", #attribute  with dictionary
+					"filename": ""}
+	genes_norms = 	{"gene_id":"normalized_count",
+					"filename": ""}
+	isoforms_norms = {"isoform_id":"normalized_count", #isoform_id == transcript id from gene.results
+						"filename": ""}
+	tissue_type = "none";
+
+	'''def add_isoforms_ids(self):
+		with open(self.genes_results['filename'], 'rb') as f: #use the file we read in to get all its necessary contents
+				print "\t\tReading in corresponding Genes and Isoforms IDs, and each iso_id normcount. Writing to Database."
+				reader = csv.reader(f, delimiter="\t")
+				for line in reader:
+					arrayify = line[3].split(",") #arrafiy is a list of transcript ids. they match to isoforms norms file.
+					self.genes_results[line[0]] = arrayify;
+	'''
+	def add_isoforms_norms(self,isoforms_norms_file): #functions to add attribute to the object...
+		path = pathToDataFiles + isoforms_norms_file;# filename passed from manifest needs it's path prepended.
+		self.isoforms_norms['filename'] = path; 			# so we use functions for the name, but when adding genes
+		print "\t\tAdded filename, but not actual data for this script version."
+		'''if (self.genes_results['filename']):
+			with open(path, 'rb') as f: #use the file we read in to get all its necessary contents
+				print "\t\tReading in Isoforms Normalized Results and storing in TissueSample in cabinet."
+				reader = csv.reader(f, delimiter="\t")
+				for line in reader:
+					self.isoforms_norms[line[0]] = line[1] #add this data to the dict attribute above
+		'''
+	def add_genes_norms(self,genes_norms_file):
+		path = pathToDataFiles + genes_norms_file;
+		self.genes_norms['filename'] = path;
+		with open(path, 'rb') as f:
+			print "\t\tReading in Genes Normalized Results and executing to DB."
+			reader = csv.reader(f, delimiter="\t")
+			tmpList = [];
+			for line in reader:
+				tmpList = [];
+				#self.genes_norms[line[0]] = line[1]
+				tmpList.extend([line[0],line[1],self.barcode,self.tissue_type]);
+				#print tmpList
+				cur.execute('INSERT INTO genes VALUES (?,?,?,?)', tmpList)
+				#con.commit()
+		#cur.execute("SELECT * FROM genes")
+		#rows = cur.fetchall()
+		#for row in rows:
+		#	print row
+		#this is where we will export our fulfilled TissueSample object to a table using sqlite.
+
+	def add_genes_results(self,genes_results_file):
+		path = pathToDataFiles + genes_results_file;
+		self.genes_results['filename'] = path;
+		print "\t\tAdded filename, but not actual data for this script version."
+
+		'''with open(path, 'rb') as f:
+			print "\t\tReading in Gene Results and adding to TissueSample in cabinet"
+			reader = csv.reader(f, delimiter="\t")
+			if (self.isoforms_norms['filename']):
+				for line in reader:
+					arrayify = line[3].split(",") #arrafiy is a list of transcript ids. they match to isoforms norms file.
+
+					self.genes_results([line[0]]) = {isoform_id[]}
+			else:
+				for line in reader:
+					arrayify = line[3].split(",") #arrafiy is a list of transcript ids. they match to isoforms norms file.
+					self.genes_results[line[0]] = arrayify;
+		'''
+
+# you can add attributes (or attr's) to your cases by defining (or def) functions to input
+# learned from: http://sthurlow.com/python/lesson08/
+
 #############################
 #Reading in the file_manifest
 #############################
@@ -178,13 +178,14 @@ with open(manifestFile, 'rb') as f:
 		if "rsem" in line[6]:#remove top 3 rows, and also two quantification filename_barcodes per barcode b/c unnecessary
 			print "\nLine", i,"from manifest. barcode:", line[5], "\n\t\tfilename: ",line[6].split("rsem.",1)[1]
 			#search the cabinet for this key
-			for l in cabinet:
+			for l in cabinet: #search the cabinet
 				if (line[5] == l.barcode): #the barcode is already in cabinet.
-					fileInCabinet = True; #change boolean for flow control.s
 					placeHolder = cabinet.index(l); #find position of the TissueSample
+					fileInCabinet = True;
 					print "\t\tFound the TissueSample in cabinet! placeHolder: ", placeHolder
-			if (fileInCabinet == True): #this needs to be after the for loop searching cabinet.
-										#b/c we search the whole cabinet for the file.
+							#this needs to be after the for loop searching cabinet.
+							#b/c we search the whole cabinet for the file.
+			if (fileInCabinet == True): # the barcode is already there!
 				if "genes.normalized" in line[6]:
 					cabinet[placeHolder].add_genes_norms(line[6])
 				elif "genes.results" in line[6]:
@@ -193,8 +194,7 @@ with open(manifestFile, 'rb') as f:
 					cabinet[placeHolder].add_isoforms_norms(line[6]);
 				fileInCabinet = False #now search is done, reset this indicator for next barcode.
 			else: #otherwise, the barcode is not in Cabinet.
-				x = TissueSample(line[5]) #We make an new instance of the object
-				x.find_tissue_type()
+				x = TissueSample(line[5]) #We make an new instance of the object, with tissue sample.
 				if "genes.normalized" in line[6]:
 					x.add_genes_norms(line[6])
 				elif "genes.results" in line[6]:
@@ -202,20 +202,20 @@ with open(manifestFile, 'rb') as f:
 				elif "isoforms.normalized" in line[6]:
 					x.add_isoforms_norms(line[6])
 				cabinet.append(x) #and add it to the cabinet.
-		else: #this means the string "rsem." is not in line[6], therefore the line isn't important for our purposes.
+		else:
 			print "\nLine",i,"does not contain a barcode or proper filename\n"
-
-cur.execute("SELECT * FROM genes")
+print "Now printing all genes and normcounts to console. They have already been printed to geneSequenceResults.db in this directory."
+print "\tThis will take about 5 minutes... "
+cur.execute("SELECT * FROM genes") #this is basic mysqlite and it's pretty simple!
 rows = cur.fetchall()
 for row in rows:
 	print row
-
+print "Above is the database printed out to console."
 #Printing the cabinet to console!
 print '-' * 55
 #for l in cabinet:
 	#print "\tcase:",cabinet.index(l),l.barcode, l.genes_results['filename'].split("rsem.",1)[1], l.genes_norms['filename'].split("rsem.",1)[1], l.isoforms_norms['filename'].split("rsem.",1)[1],l.tissue_type,'\n'
-print "Overview of Filing Cabinet w/ shortened filenames printed above."
-print '--length of cabinet: ', len(cabinet)
+print '--length of cabinet: ', len(cabinet), "tissue samples."
 
 
 con.commit()
